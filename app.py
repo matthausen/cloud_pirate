44dfdf9e4a10d75303c50aadfbff9522fe0954df
Matteo Fusilli <matteofusilli@Matteos-MacBook-Pro.local>
2020-09-09 19:08:52 +0100

    user feedback

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
 import os
 from telegram.ext import Updater, CommandHandler, ConversationHandler
+from telegram.ext.callbackqueryhandler import CallbackQueryHandler
+from telegram.callbackquery import CallbackQuery
 from telegram.parsemode import ParseMode
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from flask import Flask, render_template, request, send_from_directory
 from flask_cors import CORS
 import glob
@@ -19,6 +22,8 @@ load_dotenv(dotenv_path=env_path)
 
 TELEGRAM_TOKEN = os.getenv("TOKEN")
 
+results_cleaned = []
+
 # Download the mp3 audio from a youtube video
 def download_audio(query):
   ydl_opts = {
@@ -35,18 +40,50 @@ def download_audio(query):
   except Exception:
     return False
   
-
+# Search on YouTube and return a list of 10 results
 def search(bot, update):
   message = update.message.text
   input = message.split(' ')
   input.pop(0)
   user_input = " ".join(str(x) for x in input)
   print(f'Searching: {user_input}')
+  
   results = YoutubeSearch(user_input, max_results=10).to_dict()
-  # send results to the user (only id, thumbnails[0], title and duration).
+
+  # Clean results and return id, title and duration
   for song in results:
-    for key, value in song.items():
-      print(key, value)
+    song_cleaned={'id': song['id'], 'title': song['title'], 'duration': song['duration']}
+    results_cleaned.append(song_cleaned)
+  
+  text=''
+  for index, item in enumerate(results_cleaned):
+    if index <= 7:
+      text += "- {}. {}, {} \n".format(index+1, item['title'], item['duration'])
+  
+  
+  bot.send_message(chat_id=update.message.chat_id, text=text)
+
+  # Allow the user to select from the 8 options
+  options = []
+  for i in range(1, 9):
+    options.append(InlineKeyboardButton(i, callback_data=i))
+    
+  reply_markup = InlineKeyboardMarkup([options])
+  bot.send_message(chat_id=update.message.chat_id, text='Choose an option to download as mp3', reply_markup=reply_markup)
+
+# Handle query after user selected one option
+def query_handler(bot, update):
+    query: CallbackQuery = update.callback_query
+    query.answer()
+    query.edit_message_text(text="Downloading: {} ... Please wait".format(query.data))
+
+    song_id = results_cleaned[int(query.data)-1]['id']
+    
+    # send the audio file here
+    # Delete the previously saved files
+    download_audio(song_id)
+    for audio in glob.glob('./*mp3'):
+      bot.send_audio(chat_id=update.message.chat_id, audio=open(audio, 'rb'))
 
 
 def download(bot, update):
@@ -99,9 +136,12 @@ def main():
   dp.add_handler(CommandHandler('start',help))
   dp.add_handler(CommandHandler('search',search))
   dp.add_handler(CommandHandler('download',download))
+
+  updater.dispatcher.add_handler(CallbackQueryHandler(query_handler))
+
   updater.start_polling()
   updater.idle()
 
 
 if __name__ == '__main__':
-  main
+  main()
